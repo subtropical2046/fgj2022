239616b0cc2e6b5d0a8048a60cd5f07c03820577
WillardPeng <42560230+WillardPeng@users.noreply.github.com>
2022-09-03 15:33:41 +0800

    Import Dotween

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] private float _decideRandomMoveInterval = default;
+    [SerializeField] private int _randomMoveRate = default;
+    [SerializeField] private float _randomMoveSpeed = default;
+
+    private LightState _currentState;
+    private bool _keepRandomMove;
+    private enum LightState
+    {
+        InPlayerControl,
+        RandomMove,
+        Attracted
+    }
+    
+    private void Start()
+    {
+        _currentState = LightState.InPlayerControl;
+        _keepRandomMove = true;
+        StartCoroutine(DecideRandomMove());
+    }
+
+    private IEnumerator DecideRandomMove()
+    {
+        while (_keepRandomMove)
+        {
+            yield return new WaitForSeconds(_decideRandomMoveInterval);
+            if (Random.Range(1, 101) <= _randomMoveRate)
+            {
+                //move light transform to random place
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //暫定tag為Attractor
+        if (collision.CompareTag("Attractor"))
+        {
+            if (!(_currentState == LightState.Attracted))
+            {
+                //move light transform to target transform
+                //PlayGetAttractionAnimation(collision.transform);
+            }
+        }
+    }
+}
